f90d54cea31b4316d474826caa644a3ff40b187c
vicky <vickyempire723@gmail.com>
2024-12-27 12:42:35 +0530

    adding link on learn more button

--- src/components/home/empowering-section.tsx
+++ src/components/home/empowering-section.tsx
@@ -75,9 +75,12 @@ export function EmpoweringSection() {
               efficiency.
             </p>
             <Button variant="secondary" className="rounded-3xl">
-              Learn More
-              <ArrowRight className="w-4 h-4 ml-2" />
-            </Button>
+  <a href="/learnmore" className="flex items-center">
+    Learn More
+    <ArrowRight className="w-4 h-4 ml-2" />
+  </a>
+</Button>
+
           </div>
 
           <div className="space-y-6">
